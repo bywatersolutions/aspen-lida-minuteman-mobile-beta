9e55a8f010607646407a7c17bacbd9c258a1ed28
kirstien <kirstienkroeger@icloud.com>
2026-01-02 01:24:38 -0600

    Add modal for creating list groups

---
+++ code/src/screens/MyAccount/Lists/CreateListGroup.js
@@ -0,0 +1,130 @@
+import React, { useState } from 'react';
+import { useQueryClient } from '@tanstack/react-query';
+import { LanguageContext, LibrarySystemContext, ThemeContext, UserContext } from '../../../context/initialContext';
+import { Center, Button, ButtonIcon, ButtonText, CloseIcon, FormControl, FormControlLabel, FormControlLabelText, Heading, Icon, Input, InputField, Modal, ModalBackdrop, ModalCloseButton, ModalHeader, ModalContent, ModalBody, ButtonGroup, ModalFooter, SelectTrigger, SelectInput, SelectIcon, ChevronDownIcon, SelectPortal, SelectBackdrop, SelectContent, SelectDragIndicatorWrapper, SelectDragIndicator, SelectItem, Select } from '@gluestack-ui/themed';
+import { MaterialIcons } from '@expo/vector-icons';
+import { getTermFromDictionary } from '../../../translations/TranslationService';
+import { createListGroup } from '../../../util/api/list';
+import { popAlert } from '../../../components/loadError';
+import { Platform } from 'react-native';
+import _ from 'lodash';
+
+const CreateListGroup = (props) => {
+     const { setLoading } = props;
+     const queryClient = useQueryClient();
+     const { user, listGroups } = React.useContext(UserContext);
+     const { library } = React.useContext(LibrarySystemContext);
+     const { language } = React.useContext(LanguageContext);
+     const { textColor, theme, colorMode } = React.useContext(ThemeContext);
+     const [loading, setAdding] = React.useState(false);
+     const [showModal, setShowModal] = useState(false);
+
+     const [title, setTitle] = useState('');
+     const [nestedGroupId, setNestedGroupId] = useState("no");
+
+     let hasListGroups = false;
+     if(user.numListGroups) {
+          hasListGroups = user.numListGroups > 0;
+     }
+
+     const toggle = () => {
+          setShowModal(!showModal);
+     };
+
+     return (
+          <Center>
+               <Button onPress={toggle} size="sm" bgColor={theme['colors']['primary']['500']}>
+                    <ButtonIcon color={theme['colors']['primary']['500-text']} as={MaterialIcons} name="add" mr="$1" />
+                    <ButtonText color={theme['colors']['primary']['500-text']}>{getTermFromDictionary(language, 'create_new_list')}</ButtonText>
+               </Button>
+               <Modal isOpen={showModal} onClose={toggle} size="full" avoidKeyboard>
+                    <ModalBackdrop />
+                    <ModalContent maxWidth="90%"  bgColor={colorMode === 'light' ? theme['colors']['warmGray']['50'] : theme['colors']['coolGray']['700']}>
+                         <ModalHeader>
+                              <Heading size="md" color={textColor}>{getTermFromDictionary(language, 'create_new_list_group')}</Heading>
+                              <ModalCloseButton hitSlop={{ top: 30, bottom: 30, left: 30, right: 30 }}>
+                                   <Icon as={CloseIcon} color={textColor} />
+                              </ModalCloseButton>
+                         </ModalHeader>
+                         <ModalBody>
+                              <FormControl pb="$5">
+                                   <FormControlLabel>
+                                        <FormControlLabelText color={textColor}>{getTermFromDictionary(language, 'new_list_group_name')}</FormControlLabelText>
+                                   </FormControlLabel>
+                                   <Input borderColor={colorMode === 'light' ? theme['colors']['coolGray']['500'] : theme['colors']['gray']['300']}>
+                                        <InputField id="title" onChangeText={(text) => setTitle(text)} returnKeyType="next" defaultValue={title} color={textColor} />
+                                   </Input>
+                              </FormControl>
+                              {hasListGroups && (
+                              <FormControl pb="$5">
+                                   <FormControlLabel>
+                                        <FormControlLabelText color={textColor}>{getTermFromDictionary(language, 'should_nest_list_group')}</FormControlLabelText>
+                                   </FormControlLabel>
+                                   <Select
+                                        name="should_nest_list_group"
+                                        selectedValue={nestedGroupId}
+                                        accessibilityLabel={getTermFromDictionary(language, 'should_nest_list_group')}
+                                        mt="$1"
+                                        mb="$2"
+                                        onValueChange={(itemValue) => setNestedGroupId(itemValue)}>
+                                        <SelectTrigger variant="outline" size="md">
+                                             {nestedGroupId !== "no" && nestedGroupId !== "" ? (
+                                                  <SelectInput color={textColor} value={nestedGroupId} />
+                                             ) : (
+                                                  <SelectInput value={getTermFromDictionary(language, 'nest_within_group_no')} color={textColor} />
+                                             )}
+                                             <SelectIcon mr="$3" as={ChevronDownIcon} color={textColor} />
+                                        </SelectTrigger>
+                                        <SelectPortal>
+                                             <SelectBackdrop />
+                                             <SelectContent
+                                                  bgColor={colorMode === 'light' ? theme['colors']['warmGray']['50'] : theme['colors']['coolGray']['700']}
+                                                  pb={Platform.OS === 'android' ? insets.bottom + 16 : '$4'}
+                                             >
+                                                  <SelectDragIndicatorWrapper>
+                                                       <SelectDragIndicator />
+                                                  </SelectDragIndicatorWrapper>
+                                                  <SelectItem label={getTermFromDictionary(language, 'nest_within_group_no')} value="no" key={1} sx={{ _text: { color: textColor } }} />
+                                                  {_.map(listGroups, function (item, index, array) {
+                                                       return <SelectItem key={index} value={item.id} label={item.title} bgColor={nestedGroupId === item.id ? theme['colors']['tertiary']['300'] : ''} sx={{ _text: { color: nestedGroupId === item.id ? theme['colors']['tertiary']['500-text'] : textColor } }} />;
+                                                  })}
+                                             </SelectContent>
+                                        </SelectPortal>
+                                   </Select>
+                              </FormControl>
+                              )}
+                         </ModalBody>
+                         <ModalFooter>
+                              <ButtonGroup>
+                                   <Button variant="outline" onPress={toggle} borderColor={theme['colors']['primary']['500']}>
+                                        <ButtonText color={theme['colors']['primary']['500']}>{getTermFromDictionary(language, 'close_window')}</ButtonText>
+                                   </Button>
+                                   <Button
+                                        bgColor={theme['colors']['primary']['500']}
+                                        isLoading={loading}
+                                        isLoadingText={getTermFromDictionary(language, 'creating_list', true)}
+                                        onPress={async () => {
+                                             setAdding(true);
+                                             await createListGroup(title, nestedGroupId, library.baseUrl).then(async (res) => {
+                                                  let status = 'success';
+                                                  if (!res.success) {
+                                                       status = 'danger';
+                                                  }
+                                                  queryClient.invalidateQueries({ queryKey: ['user', library.baseUrl, language] });
+                                                  queryClient.invalidateQueries({ queryKey: ['lists', user.id, library.baseUrl, language] });
+                                                  toggle();
+                                                  setLoading(true);
+                                                  popAlert(getTermFromDictionary(language, 'list_created'), res.message, status);
+                                             });
+                                        }}>
+                                        <ButtonText color={theme['colors']['primary']['500-text']}>{getTermFromDictionary(language, 'create_list_group')}</ButtonText>
+                                   </Button>
+                              </ButtonGroup>
+                         </ModalFooter>
+                    </ModalContent>
+               </Modal>
+          </Center>
+     );
+}
+
+export default CreateListGroup;
